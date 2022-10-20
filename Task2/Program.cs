135cdaed3d197747f1cbc1e0c92d380dad6375f6
Владимир Гаврилов <saxar.1kg@mail.ru>
2022-10-20 21:30:52 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string reverseStr = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverseStr += str[i];
+            }
+            {
+                if (str == reverseStr)
+                    Console.WriteLine("Введенная строка не является палиндромом.");
+                else
+                    Console.WriteLine("Введенная строка является палиндромом.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
